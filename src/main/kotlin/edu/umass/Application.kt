5abd38021af2417fd1ff3f4cb8167b79b234a085
John Wordell <wordell.john@protonmail.com>
2023-11-06 14:20:17 -0500

    Add initial backend ktor project and connect it to the frontend

---
+++ src/main/kotlin/edu/umass/Application.kt
@@ -0,0 +1,21 @@
+package edu.umass
+
+import edu.umass.plugins.configureHTTP
+import edu.umass.plugins.configureRouting
+import edu.umass.plugins.configureSerialization
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    // configureSecurity()
+    configureSerialization()
+    configureRouting()
+    configureHTTP()
+
+}
