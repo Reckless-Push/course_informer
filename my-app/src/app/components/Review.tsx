e9c67b8e3e4c4800c36ba751551be6508f9c5cd3
Ishita <ishitac1150@gmail.com>
2023-11-18 12:58:10 -0500

    Reviews added

--- my-app/src/app/components/Review.tsx
+++ my-app/src/app/components/Review.tsx
@@ -16,30 +16,7 @@ class Review extends Component {
                             <div className={styles.RatingNumber}>3.0</div>
                         </div>
                     </div>
-                    {/* <div className={styles.total}>
-                        <div className={styles.Options}>
-                            <div className={styles.ForCredit}>
-                                <div className={styles.optionheader} >For credit:</div>
-                                <div className={styles.optionval}>Yes</div>
-                            </div>
-                            <div className={styles.ForCredit}>
-                                <div className={styles.optionheader} >For credit:</div>
-                                <div className={styles.optionval}>Yes</div>
-                            </div>
-                            <div className={styles.ForCredit}>
-                                <div className={styles.optionheader} >For credit:</div>
-                                <div className={styles.optionval}>Yes</div>
-                            </div>
-                            <div className={styles.ForCredit}>
-                                <div className={styles.optionheader} >For credit:</div>
-                                <div className={styles.optionval}>Yes</div>
-                            </div>
-
-                        </div>
-                        <div className={styles.content}>
-                            Professor Conboy is a kind and understanding instructor who cares about her students and the material she teaches. However, she has high expectations for grading. While the workload isn't terrible, it can be very easy to lose a lot of credit on the homework assignments due to unclear instructions.
-                        </div>
-                    </div> */}
+                    {/*   */}
                 </div>
             </div >
 
