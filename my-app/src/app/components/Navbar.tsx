78e354f9733b4c2777ea4f47cf4b243332a283a4
ishitachakravarthy <ishitac1150@gmail.com>
2023-12-05 11:28:58 -0500

    files deleted from main

--- my-app/src/app/components/Navbar.tsx
+++ my-app/src/app/components/Navbar.tsx
@@ -2,40 +2,48 @@
 
 import styles from "@/app/components/css/navbar.module.css";
 
-import { ComponentStates } from '@/types/ComponentStates'
+import { ComponentStates } from "@/types/ComponentStates";
 
 interface NavBarProps {
-    onToggleComponent: (component: keyof ComponentStates) => void
-    onHome: () => void
-    componentStates: {
-        courses: boolean
-        reviews: boolean
-        user:boolean
-    }
+  onToggleComponent: (component: keyof ComponentStates) => void;
+  onHome: () => void;
+  componentStates: {
+    courses: boolean;
+    reviews: boolean;
+    user: boolean;
+  };
 }
 
-function Navbar({
-    onToggleComponent,
-    onHome,
-    componentStates,
-}: NavBarProps) {
-
-    return (
-
-        <nav className={styles.nav}>
-            <div>
-                <ul>
-                    <li><a><button onClick={() => onToggleComponent('courses')}>Course</button></a></li>
-                    <li><a><button onClick={() => onToggleComponent('reviews')}>Review</button></a></li>
-                    <li><a><button onClick={() => onToggleComponent('user')}>User</button></a></li>
-                    {/* <li><a>My Profile</a></li> */}
-                    {/* <li><a>Logout</a></li> */}
-                </ul>
-            </div>
-
-        </nav>
-    );
+function Navbar({ onToggleComponent, onHome, componentStates }: NavBarProps) {
+  return (
+    <nav className={styles.nav}>
+      <div>
+        <ul>
+          <li>
+            <a>
+              <button onClick={() => onToggleComponent("courses")}>
+                Course
+              </button>
+            </a>
+          </li>
+          <li>
+            <a>
+              <button onClick={() => onToggleComponent("reviews")}>
+                Review
+              </button>
+            </a>
+          </li>
+          <li>
+            <a>
+              <button onClick={() => onToggleComponent("user")}>User</button>
+            </a>
+          </li>
+          {/* <li><a>My Profile</a></li> */}
+          {/* <li><a>Logout</a></li> */}
+        </ul>
+      </div>
+    </nav>
+  );
 }
-
 
 export default Navbar;
