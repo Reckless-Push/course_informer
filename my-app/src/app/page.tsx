a23b419a14daad15cb550752fb61bd1e4e4305cf
ishitachakravarthy <ishitac1150@gmail.com>
2023-12-04 22:31:07 -0500

    Profile page added

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,63 +1,73 @@
-'use client'
-import React, { useState } from 'react'
-import CoursePage from '@/app/course/course'
-import { ComponentStates } from '@/types/ComponentStates'
-import Navbar from '@/app/components/Navbar'
-import ResponseForm from '@/app/review/review'
+"use client";
+import React, { useState } from "react";
+import CoursePage from "@/app/course/course";
+import { ComponentStates } from "@/types/ComponentStates";
+import Navbar from "@/app/components/Navbar";
+import ResponseForm from "@/app/review/review";
 
-import useFetchData from './hooks/useFetchData'
-import { CourseResponse } from '@/types/course'
-import ProfilePage from '@/app/user/user'
-type ComponentKey = 'courses' | 'reviews' |'user'
+import useFetchData from "./hooks/useFetchData";
+import { CourseResponse } from "@/types/course";
+import ProfilePage from "@/app/user/user";
+type ComponentKey = "courses" | "reviews" | "user";
 
 const Home = () => {
-    const [componentStates, setComponentStates] = useState<ComponentStates>({
-        courses: false,
-        reviews: false,
-        user:false
-    })
+  const [componentStates, setComponentStates] = useState<ComponentStates>({
+    courses: false,
+    reviews: false,
+    user: false,
+  });
 
-    const onToggleComponent = (component: ComponentKey) => {
-        setComponentStates((prev) => ({ ...prev, [component]: !prev[component] }))
-    }
+  const onToggleComponent = (component: ComponentKey) => {
+    setComponentStates((prev) => ({ ...prev, [component]: !prev[component] }));
+  };
 
-    const onHome = () => {
-        setComponentStates({
-            courses: false,
-            reviews: false,
-            user:false,
-        })
-    }
-     const {
-        data: courseData,
-        loading: courseLoading,
-        error: courseError
-    } = useFetchData<CourseResponse>('https://localhost:8443/course');
+  const onHome = () => {
+    setComponentStates({
+      courses: false,
+      reviews: false,
+      user: false,
+    });
+  };
+  const {
+    data: courseData,
+    loading: courseLoading,
+    error: courseError,
+  } = useFetchData<CourseResponse>("https://localhost:8443/course");
 
-    return (
-        <div>
-            <Navbar
-                onToggleComponent={onToggleComponent}
-                onHome={onHome}
-                componentStates={componentStates}
-            />
+  return (
+    <div>
+      <Navbar
+        onToggleComponent={onToggleComponent}
+        onHome={onHome}
+        componentStates={componentStates}
+      />
 
-            {/* <Navbar></Navbar> */}
-            {
-            componentStates.courses &&courseData &&
-            <CoursePage onToggleComponent={onToggleComponent} onHome={onHome} componentStates={componentStates} courseData={courseData.course_table[0]}/>
-            }
+      {/* <Navbar></Navbar> */}
+      {componentStates.courses && courseData && (
+        <CoursePage
+          onToggleComponent={onToggleComponent}
+          onHome={onHome}
+          componentStates={componentStates}
+          courseData={courseData.course_table[0]}
+        />
+      )}
 
-            {
-            componentStates.reviews && 
-            <ResponseForm onToggleComponent={onToggleComponent} onHome={onHome}componentStates={componentStates} />
-            }
-             {
-            componentStates.user && 
-            <ProfilePage onToggleComponent={onToggleComponent} onHome={onHome}componentStates={componentStates} />
-            }
-        </div>
-    )
-}
+      {componentStates.reviews && (
+        <ResponseForm
+          onToggleComponent={onToggleComponent}
+          onHome={onHome}
+          componentStates={componentStates}
+        />
+      )}
+      {componentStates.user && (
+        <ProfilePage
+          onToggleComponent={onToggleComponent}
+          onHome={onHome}
+          componentStates={componentStates}
+        />
+      )}
+    </div>
+  );
+};
 
-export default Home
+export default Home;
--- my-app/src/app/review/review.tsx
+++ my-app/src/app/review/review.tsx
@@ -1,119 +1,246 @@
 "use client";
 
 import styles from "@/app/review/review.module.css";
-import { ComponentStates } from '@/types/ComponentStates'
+import { ComponentStates } from "@/types/ComponentStates";
 
 interface ResponseFormProps {
-    onToggleComponent: (component: keyof ComponentStates) => void
-    onHome: () => void
-    componentStates: {
-        courses: boolean
-        reviews: boolean
+  onToggleComponent: (component: keyof ComponentStates) => void;
+  onHome: () => void;
+  componentStates: {
+    courses: boolean;
+    reviews: boolean;
+    user: boolean;
+  };
+}
+
+function handleClick({
+  onToggleComponent,
+  onHome,
+  componentStates,
+}: ResponseFormProps) {
+  onHome();
+  onToggleComponent("reviews");
+}
+
+import React, { useEffect, useState } from "react";
+import {
+  Box,
+  Button,
+  Checkbox,
+  FormControlLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  TextField,
+  Typography,
+} from "@mui/material";
+import usePostData from "@/app/hooks/usePostData";
+import useFetchData from "@/app/hooks/useFetchData";
+import { Course, CourseResponse } from "@/types/course";
+import { Professor, ProfessorResponse } from "@/types/professor";
+import { Review } from "@/types/review";
+
+function ResponseForm({
+  onToggleComponent,
+  onHome,
+  componentStates,
+}: ResponseFormProps) {
+  const { data: coursesData } = useFetchData<CourseResponse>(
+    "https://localhost:8443/course"
+  );
+  const { data: professorsData } = useFetchData<ProfessorResponse>(
+    "https://localhost:8443/professor"
+  );
+  const initialReviewState: Review = {
+    id: 0,
+    professor: null,
+    course: null,
+    userId: null,
+    date: null,
+    difficulty: 0,
+    quality: 0,
+    comment: "",
+    fromRmp: false,
+    forCredit: false,
+    attendance: false,
+    textbook: false,
+    grade: "C",
+  };
+
+  const [review, setReview] = useState<Review>(initialReviewState);
+  const [selectedCourse, setSelectedCourse] = useState<string>("");
+  const [selectedProfessor, setSelectedProfessor] = useState<string>("");
+  const [isSubmitClicked, setIsSubmitClicked] = useState(false);
+  const { data, loading, error } = usePostData<Review, Review>(
+    "https://localhost:8443/review",
+    review,
+    isSubmitClicked
+  );
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setReview({ ...review, [e.target.name]: e.target.value });
+  };
+
+  const handleCourseChange = (event: SelectChangeEvent) => {
+    setSelectedCourse(event.target.value as string);
+  };
+
+  const handleProfessorChange = (event: SelectChangeEvent) => {
+    setSelectedProfessor(event.target.value as string);
+  };
+
+  useEffect(() => {
+    if (data) {
+      setIsSubmitClicked(false);
     }
-}
+  }, [data]);
 
-function handleClick({onToggleComponent,onHome,componentStates}: ResponseFormProps) {
-    onHome();
-    onToggleComponent('reviews');
-}
-
-function ResponseForm({onToggleComponent,onHome,componentStates,}: ResponseFormProps) {
-    return (
-        <div>
-            {/* <Navbar></Navbar> */}
-            <div className={styles.main}>
-                <div className={styles.content}>
-                    <div className={styles.CourseName}>COMPSCI 520 Thry + Practice; Software Engin</div>
-                    <form className={styles.FormContainer}>
-                        <div className={styles.SelectProfessor}>
-                            <label className={styles.Text}>Select Professor</label>
-                            <select id="Professor">
-                                <option value="null"></option>
-                                <option value="HeatherConboy">Heather Conboy</option>
-                                <option value="JuanZhai">Juan Zhai</option>
-                            </select>
-                        </div>
-                        <div className={styles.SelectProfessor}>
-                            <label className={styles.Text}>Rate your Professor</label>
-                            <input type="range" min="1" max="5" step="1" defaultValue="4" className={styles.slider} id="myRange"></input>
-                        </div>
-                        <div className={styles.SelectProfessor}>
-                            <label className={styles.Text}>How difficult was this Professor</label>
-                            <input type="range" min="1" max="5" step="1" defaultValue="4" className={styles.slider} id="myRange"></input>
-                        </div>
-                        <div className={styles.RetakeCourse}>
-                            <label className={styles.Text}>Would you retake this Course?</label>
-                            <label className={styles.retake}>
-                                <input type="radio" id={styles.RetakeCourseRadio} value="Yes" name="retake"></input>
-                                <span className={styles.checkmark}></span>Yes
-                            </label>
-                            <label className={styles.retake}>
-                                <input type="radio" id={styles.RetakeCourseRadio} value="No" name="retake"></input>
-                                <span className={styles.checkmark}></span> No
-                            </label>
-                        </div>
-                        <div className={styles.SelectProfessor}>
-                            <label className={styles.Text}>Was this course taken for credit</label>
-                            <label className={styles.retake}>
-                                <input type="radio" id={styles.RetakeCourseRadio} value="Yes" name="credit"></input>
-                                <span className={styles.checkmark}></span>Yes
-                            </label>
-                            <label className={styles.retake}>
-                                <input type="radio" id={styles.RetakeCourseRadio} value="No" name="credit"></input>
-                                <span className={styles.checkmark}></span> No
-                            </label>
-                        </div>
-                        <div className={styles.SelectProfessor}>
-                            <label className={styles.Text}>Did this professor use textbooks</label>
-                            <label className={styles.retake}>
-                                <input type="radio" id={styles.RetakeCourseRadio} value="Yes" name="textbook"></input>
-                                <span className={styles.checkmark}></span>Yes
-                            </label>
-                            <label className={styles.retake}>
-                                <input type="radio" id={styles.RetakeCourseRadio} value="No" name="textbook"></input>
-                                <span className={styles.checkmark}></span> No
-                            </label>
-                        </div>
-                        <div className={styles.SelectProfessor}>
-                            <label className={styles.Text}>Was attendance mandatory?</label>
-                            <label className={styles.retake}>
-                                <input type="radio" id={styles.RetakeCourseRadio} value="Yes" name="attendance"></input>
-                                <span className={styles.checkmark}></span>Yes
-                            </label>
-                            <label className={styles.retake}>
-                                <input type="radio" id={styles.RetakeCourseRadio} value="No" name="attendance"></input>
-                                <span className={styles.checkmark}></span> No
-                            </label>
-                        </div>
-                        <div className={styles.SelectProfessor}>
-                            <label className={styles.Text}>Select grade</label>
-                            {/* <input list="browsers" name="browser" /> */}
-                            <select id="grade">
-                                <option value="null"></option>
-                                <option value="A">A</option>
-                                <option value="A-">A-</option>
-                                <option value="B">B</option>
-                                <option value="B-">B-</option>
-                                <option value="C">C</option>
-                            </select>
-                        </div>
-                        <div className={styles.addComments}>
-                            <label className={styles.Text} >Additional comments</label>
-                            <textarea className={styles.addCommentstext} placeholder="Add more feedback">
-                            </textarea>
-                        </div>
-                        <div className={styles.RateThisCourse}>
-                            <button className={styles.RateThisCourseBtn}
-                             onClick={() => handleClick({onToggleComponent,onHome,componentStates,})}>
-                            Rate this Course</button>
-                        </div>
-                    </form>
-                </div>
-            </div >
-
-        </div >
-
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    const selectedProfessorObject = professorsData?.professor_table.find(
+      (prof) => prof.firstName === selectedProfessor
     );
+
+    const selectedCourseObject = coursesData?.course_table.find(
+      (course) => course.name === selectedCourse
+    );
+
+    setReview({
+      ...review,
+      professor: selectedProfessorObject ? selectedProfessorObject : null,
+      course: selectedCourseObject ? selectedCourseObject : null,
+    });
+    console.log(review); // Log the review object
+    setIsSubmitClicked(true);
+  };
+
+  return (
+    <Box
+      component="form"
+      onSubmit={handleSubmit}
+      className="bg-gradient-to-r from-[rgb(var(--background-start-rgb))] to-[rgb(var(--background-end-rgb))] text-white p-4 rounded-lg shadow-md pad10 mTop10"
+    >
+      <Select
+        className="mb-4 w-full bg-[#FFFFFF] text-[#000000] border border-[rgb(var(--neutral-dark-gray-rgb))] rounded"
+        value={selectedProfessor}
+        onChange={handleProfessorChange}
+      >
+        {professorsData?.professor_table.map((professor: Professor) => (
+          <MenuItem key={professor.id} value={professor.firstName}>
+            {professor.firstName} {professor.lastName}
+          </MenuItem>
+        ))}
+      </Select>
+      <Select
+        className="mb-4 w-full bg-[#FFFFFF] text-[#000000] border border-[rgb(var(--neutral-dark-gray-rgb))] rounded"
+        value={selectedCourse}
+        onChange={handleCourseChange}
+      >
+        {coursesData?.course_table.map((course: Course) => (
+          <MenuItem key={course.cicsId} value={course.name}>
+            {course.name}
+          </MenuItem>
+        ))}
+      </Select>
+      <TextField
+        className="mb-4 w-full bg-[#FFFFFF] text-[#000000] border border-[rgb(var(--neutral-dark-gray-rgb))] rounded"
+        name="difficulty"
+        label="Difficulty"
+        type="number"
+        value={review.difficulty}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+        id="difficulty"
+        autoComplete="5"
+        required
+      />
+      <TextField
+        className="mb-4 w-full bg-[#FFFFFF] text-[#000000] border border-[rgb(var(--neutral-dark-gray-rgb))] rounded"
+        name="quality"
+        label="Quality"
+        type="number"
+        value={review.quality}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+        id="quality"
+        autoComplete="3"
+        required
+      />
+      <TextField
+        className="mb-4 w-full bg-[#FFFFFF] text-[#000000] border border-[rgb(var(--neutral-dark-gray-rgb))] rounded"
+        name="comment"
+        label="Comment"
+        type="text"
+        value={review.comment}
+        onChange={handleChange}
+        fullWidth
+        margin="normal"
+        id="comment"
+        autoComplete="This course is great!"
+        required
+      />
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={review.fromRmp}
+            onChange={(e) =>
+              setReview({ ...review, fromRmp: e.target.checked })
+            }
+            name="fromRmp"
+          />
+        }
+        label="From Rate My Professor"
+      />
+
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={review.forCredit}
+            onChange={(e) =>
+              setReview({ ...review, forCredit: e.target.checked })
+            }
+            name="forCredit"
+          />
+        }
+        label="For Credit"
+      />
+
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={review.attendance}
+            onChange={(e) =>
+              setReview({ ...review, attendance: e.target.checked })
+            }
+            name="attendance"
+          />
+        }
+        label="Attendance Required"
+      />
+
+      <FormControlLabel
+        control={
+          <Checkbox
+            checked={review.textbook}
+            onChange={(e) =>
+              setReview({ ...review, textbook: e.target.checked })
+            }
+            name="textbook"
+          />
+        }
+        label="Textbook Required"
+      />
+
+      <Button type="submit" fullWidth variant="contained" sx={{ mt: 3, mb: 2 }}>
+        Submit
+      </Button>
+      {loading && <Typography>Loading...</Typography>}
+      {error && <Typography color="error">Error: {error.message}</Typography>}
+      {data && <Typography>Success!</Typography>}
+    </Box>
+  );
 }
 
 export default ResponseForm;
