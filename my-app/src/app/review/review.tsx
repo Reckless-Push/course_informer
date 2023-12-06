0a7c9aec75f1b0dbcd4a5aeb96f0c316167ce669
ishitachakravarthy <ishitac1150@gmail.com>
2023-12-06 12:53:05 -0500

    Review form UI update

--- my-app/src/app/review/review.tsx
+++ my-app/src/app/review/review.tsx
@@ -57,17 +57,18 @@ const ResponseForm = ({
     course: null,
     userId: null,
     date: null,
-    difficulty: 0,
-    quality: 0,
+    difficulty: 5,
+    quality: 5,
     comment: "",
     fromRmp: false,
     forCredit: false,
     attendance: false,
     textbook: false,
-    grade: "C",
+    grade: "A",
   };
 
   const [review, setReview] = useState<Review>(initialReviewState);
+  const [selectedGrade, setSelectedGrade] = useState<string>("");
   const [selectedCourse, setSelectedCourse] = useState<string>("");
   const [selectedProfessor, setSelectedProfessor] = useState<string>("");
   const [isSubmitClicked, setIsSubmitClicked] = useState(false);
@@ -84,11 +85,13 @@ const ResponseForm = ({
   );
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(e.target.value);
     setReview({ ...review, [e.target.name]: e.target.value });
   };
 
-  const handleCourseChange = (event: SelectChangeEvent) => {
-    setSelectedCourse(event.target.value as string);
+  const handleGradeChange = (event: SelectChangeEvent) => {
+    console.log(event.target.value);
+    setSelectedGrade(event.target.value as string);
   };
 
   const handleProfessorChange = (event: SelectChangeEvent) => {
@@ -113,39 +116,41 @@ const ResponseForm = ({
       ...review,
       professor: selectedProfessorObject ? selectedProfessorObject : null,
       course: selectedCourseObject ? selectedCourseObject : null,
+      // grade: selectedGradeObject ? selectedCourseObject : null,
     });
     console.log(review); // Log the review object
     setIsSubmitClicked(true);
   };
-  const grade = ["A", "A-", "B", "B-"];
+  const grade = ["A", "A-", "B", "B-", "C+", "C", "C-", "D+", "D", "F"];
   return (
     <div>
       {/* <Navbar></Navbar> */}
       <div className={styles.main}>
-        <div className={styles.CourseName}>
-          COMPSCI 520 Thry + Practice; Software Engin
-        </div>
-        <Box
-          component="form"
-          className={styles.FormContainer}
-          onSubmit={handleSubmit}
-        >
-          <div className={styles.SelectProfessor}>
-            <label className={styles.Text}>Select Professor</label>
-            <Select
-              className={styles.prof}
-              value={selectedProfessor}
-              onChange={handleProfessorChange}
-              id="Professor"
-            >
-              {professorsData?.professor_table.map((professor: Professor) => (
-                <MenuItem key={professor.id} value={professor.firstName}>
-                  {professor.firstName} {professor.lastName}
-                </MenuItem>
-              ))}
-            </Select>
+        <div className={styles.content}>
+          <div className={styles.CourseName}>
+            COMPSCI 520 Thry + Practice; Software Engin
+          </div>
+          <Box
+            component="form"
+            className={styles.FormContainer}
+            onSubmit={handleSubmit}
+          >
+            <div className={styles.SelectProfessor}>
+              <label className={styles.Text}>Select Professor</label>
+              <Select
+                className={styles.prof}
+                value={selectedProfessor}
+                onChange={handleProfessorChange}
+                id="Professor"
+              >
+                {professorsData?.professor_table.map((professor: Professor) => (
+                  <MenuItem key={professor.id} value={professor.firstName}>
+                    {professor.firstName} {professor.lastName}
+                  </MenuItem>
+                ))}
+              </Select>
 
-            {/* <Select
+              {/* <Select
               className="mb-4 w-full bg-[#FFFFFF] text-[#000000] border border-[rgb(var(--neutral-dark-gray-rgb))] rounded"
               value={selectedCourse}
               onChange={handleCourseChange}
@@ -156,35 +161,43 @@ const ResponseForm = ({
                 </MenuItem>
               ))}
             </Select> */}
-          </div>
-          <div className={styles.SelectProfessor}>
-            <label className={styles.Text}>Rate your Professor</label>
-            <input
-              type="range"
-              min="1"
-              max="5"
-              step="1"
-              defaultValue="4"
-              className={styles.slider}
-              id="myRange"
-            ></input>
-          </div>
-          <div className={styles.SelectProfessor}>
-            <label className={styles.Text}>
-              How difficult was this Professor
-            </label>
-            <input
-              type="range"
-              min="1"
-              max="5"
-              step="1"
-              defaultValue="4"
-              className={styles.slider}
-              id="myRange"
-            ></input>
-          </div>
+            </div>
+            <div className={styles.SelectProfessor}>
+              <label className={styles.Text}>Rate your Professor</label>
+              <input
+                type="range"
+                min="1"
+                max="5"
+                step="1"
+                defaultValue="4"
+                className={styles.slider}
+                id="quality"
+                onChange={handleChange}
+                required
+                name="quality"
+                value={review.quality}
+              ></input>
+            </div>
+            <div className={styles.SelectProfessor}>
+              <label className={styles.Text}>
+                How difficult was this Professor
+              </label>
+              <input
+                type="range"
+                min="1"
+                max="5"
+                step="1"
+                defaultValue="4"
+                className={styles.slider}
+                id="difficulty"
+                onChange={handleChange}
+                required
+                name="difficulty"
+                value={review.difficulty}
+              ></input>
+            </div>
 
-          {/* <div className={styles.SelectProfessor}>
+            {/* <div className={styles.SelectProfessor}>
                 <label className={styles.Text}>Was attendance mandatory?</label>
                 <label className={styles.retake}>
                   <input
@@ -205,122 +218,104 @@ const ResponseForm = ({
                   <span className={styles.checkmark}></span> No
                 </label>
               </div> */}
-          <div className={styles.SelectProfessor}>
-            <label className={styles.Text}>Select grade</label>
-            {/* <input list="browsers" name="browser" /> */}
-            <Select className={styles.grades} value={selectedCourse} id="grade">
-              {grade.map((value, index) => (
-                <MenuItem key={index} value={value}>
-                  {value}
-                </MenuItem>
-              ))}
-            </Select>
-          </div>
-          <div className={styles.addComments}>
-            <label className={styles.Text}>Additional comments</label>
-            <TextField
-              className={styles.addCommentstext}
-              name="comment"
-              type="text"
-              value={review.comment}
-              onChange={handleChange}
-              id="comment"
-              autoComplete="This course is great!"
-              required
-              fullWidth
-            />
-          </div>
-          <div className={styles.SelectProfessor}>
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={review.forCredit}
-                  onChange={(e) =>
-                    setReview({ ...review, forCredit: e.target.checked })
-                  }
-                  name="forCredit"
-                />
-              }
-              label="For Credit"
-            />
+            <div className={styles.SelectProfessor}>
+              <label className={styles.Text}>Select grade</label>
+              {/* <input list="browsers" name="browser" /> */}
+              <Select
+                className={styles.grades}
+                value={review.grade}
+                onChange={(e) =>
+                  setReview({ ...review, grade: e.target.value })
+                }
+                id="grade"
+              >
+                {grade.map((value, index) => (
+                  <MenuItem key={index} value={value}>
+                    {value}
+                  </MenuItem>
+                ))}
+              </Select>
+            </div>
+            <div className={styles.addComments}>
+              <label className={styles.Text}>Additional comments</label>
+              <TextField
+                className={styles.addCommentstext}
+                name="comment"
+                type="text"
+                value={review.comment}
+                onChange={handleChange}
+                id="comment"
+                autoComplete="This course is great!"
+                required
+                fullWidth
+              />
+            </div>
+            <div className={styles.SelectProfessor}>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={review.forCredit}
+                    onChange={(e) =>
+                      setReview({ ...review, forCredit: e.target.checked })
+                    }
+                    name="forCredit"
+                  />
+                }
+                label="For Credit"
+              />
 
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={review.attendance}
-                  onChange={(e) =>
-                    setReview({ ...review, attendance: e.target.checked })
-                  }
-                  name="attendance"
-                />
-              }
-              label="Attendance Required"
-            />
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={review.attendance}
+                    onChange={(e) =>
+                      setReview({ ...review, attendance: e.target.checked })
+                    }
+                    name="attendance"
+                  />
+                }
+                label="Attendance Required"
+              />
 
-            <FormControlLabel
-              control={
-                <Checkbox
-                  checked={review.textbook}
-                  onChange={(e) =>
-                    setReview({ ...review, textbook: e.target.checked })
-                  }
-                  name="textbook"
-                />
-              }
-              label="Textbook Required"
-            />
-          </div>
+              <FormControlLabel
+                control={
+                  <Checkbox
+                    checked={review.textbook}
+                    onChange={(e) =>
+                      setReview({ ...review, textbook: e.target.checked })
+                    }
+                    name="textbook"
+                  />
+                }
+                label="Textbook Required"
+              />
+            </div>
 
-          <TextField
-            className="mb-4 w-full bg-[#FFFFFF] text-[#000000] border border-[rgb(var(--neutral-dark-gray-rgb))] rounded"
-            name="quality"
-            label="Quality"
-            type="number"
-            value={review.quality}
-            onChange={handleChange}
-            fullWidth
-            margin="normal"
-            id="quality"
-            autoComplete="3"
-            required
-          />
-          <TextField
-            className="mb-4 w-full bg-[#FFFFFF] text-[#000000] border border-[rgb(var(--neutral-dark-gray-rgb))] rounded"
-            name="difficulty"
-            label="Difficulty"
-            type="number"
-            value={review.difficulty}
-            onChange={handleChange}
-            fullWidth
-            margin="normal"
-            id="difficulty"
-            autoComplete="5"
-            required
-          />
-          <div className={styles.RateThisCourse}>
-            <Button
-              type="submit"
-              variant="contained"
-              // onClick={() =>
-              //   handleClick({
-              //     onToggleComponent,
-              //     onHome,
-              //     componentStates,
-              //     courseData,
-              //   })
-              // }
-              className={styles.RateThisCourseBtn}
-            >
-              Rate this Course
-            </Button>
-          </div>
+            <div className={styles.RateThisCourse}>
+              <Button
+                type="submit"
+                variant="contained"
+                // onClick={() =>
+                //   handleClick({
+                //     onToggleComponent,
+                //     onHome,
+                //     componentStates,
+                //     courseData,
+                //   })
+                // }
+                className={styles.RateThisCourseBtn}
+              >
+                Rate this Course
+              </Button>
+            </div>
 
-          {loading && <Typography>Loading...</Typography>}
-          {error && (
-            <Typography color="error">Error: {error.message}</Typography>
-          )}
-          {data && <Typography>Success!</Typography>}
-        </Box>
+            {loading && <Typography>Loading...</Typography>}
+            {error && (
+              <Typography color="error">Error: {error.message}</Typography>
+            )}
+            {data && <Typography>Success!</Typography>}
+          </Box>
+        </div>
       </div>
     </div>
   );
