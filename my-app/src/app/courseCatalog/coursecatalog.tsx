5c2184890ef7df914e27745f6cabc51b430da1b4
ishitachakravarthy <ishitac1150@gmail.com>
2023-12-05 19:59:42 -0500

    Merge issue fixed

---
+++ my-app/src/app/courseCatalog/coursecatalog.tsx
@@ -0,0 +1,40 @@
+"use client";
+import React from "react";
+import "./courseCatalog.css";
+import CourseList from "../components/CourseList";
+import CourseFiltersList from "../components/CourseFiltersList";
+import { ComponentStates } from "@/types/ComponentStates";
+
+interface courseCatalogPage {
+  onToggleComponent: (component: keyof ComponentStates) => void;
+  onHome: () => void;
+  componentStates: {
+    courses: boolean;
+    reviews: boolean;
+    courseDashboard: boolean;
+    login: boolean;
+  };
+}
+
+function courseCatalogPage({
+  onToggleComponent,
+  onHome,
+  componentStates,
+}: courseCatalogPage) {
+  return (
+    <div className="container">
+      <div className="filters_column">
+        <CourseFiltersList />
+      </div>
+      <div className="list_column">
+        <CourseList
+          onToggleComponent={onToggleComponent}
+          onHome={onHome}
+          componentStates={componentStates}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default courseCatalogPage;
