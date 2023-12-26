48b31c793929fd5d8ae3c7da14ccc17c47827fd2
John Wordell <wordell.john@protonmail.com>
2023-12-26 01:10:46 -0500

    Update title and description

--- my-app/src/types/_app.tsx
+++ my-app/src/types/_app.tsx
@@ -2,10 +2,14 @@ import React from "react";
 import "../styles/globals.css";
 import "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>CICS Course Informer</title>
+      </Head>
       <Component {...pageProps} />
     </>
   );
